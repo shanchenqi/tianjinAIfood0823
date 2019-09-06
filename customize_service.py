2d4b90fa578aa7ed6818a898d53afd24b160a340
shanchenqi <582533558@qq.com>
2019-09-06 23:27:10 +0800

    third commit

--- customize_service.py
+++ customize_service.py
@@ -241,9 +241,6 @@ class BasicBlock(nn.Module):
         return out
 
 
-
-
-
 class Bottleneck(nn.Module):
 
     expansion = 4
@@ -253,19 +250,12 @@ class Bottleneck(nn.Module):
     def __init__(self, inplanes, planes, stride=1, downsample=None):
 
         super(Bottleneck, self).__init__()
-
         self.conv1 = conv1x1(inplanes, planes)
-
         self.bn1 = nn.BatchNorm2d(planes)
-
         self.conv2 = conv3x3(planes, planes, stride)
-
         self.bn2 = nn.BatchNorm2d(planes)
-
         self.conv3 = conv1x1(planes, planes * self.expansion)
-
         self.bn3 = nn.BatchNorm2d(planes * self.expansion)
-
         self.relu = nn.ReLU(inplace=True)
 
         self.downsample = downsample
@@ -273,12 +263,9 @@ class Bottleneck(nn.Module):
         self.stride = stride
 
 
-
     def forward(self, x):
 
         identity = x
-
-
 
         out = self.conv1(x)
 
@@ -286,15 +273,10 @@ class Bottleneck(nn.Module):
 
         out = self.relu(out)
 
-
-
         out = self.conv2(out)
 
         out = self.bn2(out)
-
         out = self.relu(out)
-
-
 
         out = self.conv3(out)
 
@@ -305,9 +287,6 @@ class Bottleneck(nn.Module):
         if self.downsample is not None:
 
             identity = self.downsample(x)
-
-
-
         out += identity
 
         out = self.relu(out)
